7af4632f33d179c6518f081f061550b6e5028ae8
Oleksanda <njorogealex279@gmail.com>
2023-01-10 08:27:05 -0800

    unused

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,8 @@
  * @argv: an array of a command listed
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	print("%s\n, *argv");
+	printf("%s\n, *argv");
+	return (0);
 }
