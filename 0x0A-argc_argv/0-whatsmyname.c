b9b4b141c2395dcfb23e1571502977d4514885b0
Oleksanda <njorogealex279@gmail.com>
2023-01-10 07:43:40 -0800

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	print("%s\n, *argv");
+}
